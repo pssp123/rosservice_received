a449957de0e78e1bec674245ce3e562e207af958
pssp123 <380870978@qq.com>
2018-05-15 19:47:33 +0800

    fix():fix received srv invalied

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,18 @@
 
 #include "main.hpp"
 bool motor_ctrl_cb(rosservice_send::motor::Request &req, rosservice_send::motor::Response &res){
-	ROS_ERROR("received");
 	if(req.isOpen){
-		res.reply = "Received motor open request";
 		res.isSuccess = true;
+		res.reply = "it has received the signal";
 		ROS_INFO("open motor succeed");
 		return true;
 	}
-	return true;
+	return false;
 }
 int main(int argc, char* argv[]){
 	ros::init(argc, argv, "rosservice_received");
-	ros::NodeHandle	nh_("~");
+	ros::NodeHandle	nh_;
 	ros::ServiceServer motor_service= nh_.advertiseService("motor_ctrl", motor_ctrl_cb);
-	while(ros::ok());
 	ros::spin();
 	return 0;
 }
